30ac7006f4bd5927a23bc613e22f5d1086572ccb
RupakSTHA <30586845+RupakSTHA@users.noreply.github.com>
2023-10-11 08:42:45 -0700

    Update load-more.js

--- Paginates/load-more/load-more.js
+++ Paginates/load-more/load-more.js
@@ -1,45 +1,56 @@
 jQuery(document).ready( function($) {
 
-$(".load-more").click( function(e) {
-   e.preventDefault(); 
-    $("#load-more-form").submit(); 
-});
+    $(".load-more").click( function(e) {
+       e.preventDefault(); 
+        $("#load-more-form").submit(); 
+    });
 
-$("#load-more-form").on( 'submit', function(e){
-    e.preventDefault();
+    $("#load-more-form").on( 'submit', function(e){
+        e.preventDefault();
 
-    let formData = new FormData( this );
-    let currentPage = formData.get('currentPage');
-    //increase the currentpaged value
-    $('input[name="currentPage"]').val(++currentPage);
+        let formData = new FormData( this );
+        let currentPage = formData.get('currentPage');
+        //increment first and then save value
+        $('input[name="currentPage"]').val(++currentPage);
 
-    $.ajax({
-        url : myAjax.ajaxurl+"?action=load_more_action",
-        xhrFields: {
-            withCredentials: true
-        },
-        type : "post",
-        data : formData,
-        contentType: false,
-        processData: false,
-        beforeSend: function() {
-            $(".load-more button").html('Loading...');
-            $('.error').html('');
-        },
-        success: function(response) {
-            $html = JSON.parse(response);
-            if($html.status===true) {
-                $(".post-lists").append($html.data);
-                $(".load-more button").html('Load More');
-            }else{
-                $(".load-more").html('');
-                $('.error').html($html.data);
+        //SET AJAX ACTION FROM POST FIELD METHOD
+        formData.append("action", "rupak_load_more_action")
+
+        $.ajax({
+            //OR YOU CAN SET AJAX ACTION REQUEST FROM GET METHOD
+            // url : myAjax.ajaxurl+"?action=rupak_load_more_action",
+            url : myAjax.ajaxurl,
+            xhrFields: {
+                withCredentials: true
+            },
+            type : "post",
+            data : formData,
+            contentType: false,
+            processData: false,
+            beforeSend: function() {
+                $(".load-more button").html('Loading...');
+                $('.error').html('');
+            },
+            success: function(response) {
+                $html = JSON.parse(response);
+                if($html.status===true) {
+
+                    $(".post-lists").append($html.data);
+                    if($html.loadButton===true) {
+                        $(".load-more button").html('Load More');
+                    }else{
+                        $(".load-more").html('');
+                    }
+                    
+                }else{
+                    $(".load-more").html('');
+                    $('.error').html($html.data);
+                }
+            },
+            complete: function() {
+                // $('.loader').hide();
             }
-        },
-        complete: function() {
-            // $('.loader').hide();
-        }
-    })  
-});
-
-});
+        })  
+    });
+ 
+ });
