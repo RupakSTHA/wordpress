3c9a015567f8aa0b9f34b17668100bab77a01e9c
RupakSTHA <30586845+RupakSTHA@users.noreply.github.com>
2023-10-11 08:43:43 -0700

    Update load-more.php

--- Paginates/load-more/load-more.php
+++ Paginates/load-more/load-more.php
@@ -1,12 +1,11 @@
-
 <?php
 /**
-Template Name: Load More Template
+Template Name: Test Page
 */
 get_header();
 
 /* Paged Refresh Url Pagination Example */ 
-$perPage = 6;
+$perPage = 4;
 $currentPage = 1;
 
 $args = array(
@@ -16,7 +15,7 @@ $args = array(
 );
 
 $query = new WP_Query( $args );
-$total_posts = $query->found_posts;
+$total_posts = $query->found_posts;//10
 $totalPages = ceil($total_posts / $perPage);
 ?>
 <div class="error"></div>
@@ -38,12 +37,11 @@ if ($total_posts > $perPage) { ?>
 <div class="load-more">
     <button>Load More</button>
 </div>
-
-<!-- set variables inside form -->
 <form action="" id="load-more-form" method="POST">
-    <input type="hidden" name="load_more_per_page" value="<?php echo $perPage; ?>" /> 
-    <input type="hidden" name="currentPage" value="<?php echo ++$currentPage; ?>" /> 
+    <!-- SET AUTOCOMPLETE OFF FOR NOT CACHING FORM VALUES -->
+    <input type="hidden" name="load_more_per_page" value="<?php echo $perPage; ?>" autocomplete="off" /> 
+    <input type="hidden" name="currentPage" value="<?php echo ++$currentPage; ?>" autocomplete="off" /> 
     <?php wp_nonce_field( 'load_more_my_action', 'load_more_nonce_field' ); ?>
 </form>
 <?php } 
-get_footer();
+get_footer(); 
