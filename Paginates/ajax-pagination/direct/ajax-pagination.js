3817d7d3a4e2b9f9a592ad055721d79bf0d4d477
RupakSTHA <30586845+RupakSTHA@users.noreply.github.com>
2024-01-10 21:28:40 +0545

    Update ajax-pagination.js

--- Paginates/ajax-pagination/direct/ajax-pagination.js
+++ Paginates/ajax-pagination/direct/ajax-pagination.js
@@ -1,17 +1,37 @@
 jQuery(document).ready( function($) {
-    
+
+  const openUrl = (link, pushTrigger=true) => {
+
+    $.get( link, function( data ) {
+      let portfolio = $( data ).find( '.portfolio' );
+      $( '.portfolio-wrapper' ).html( portfolio );
+
+      if(pushTrigger){
+        window.history.pushState({href: link}, '');
+      }
+
+    });
+
+  }
+  
+  //link click and trigger history push
   $( document ).on( 'click', '.pagination a', function( e ) {
     e.preventDefault();
 
-    var link = $( this ).attr( 'href' );
-
-    $.get( link, function( data ) {
-      var portfolio = $( data ).find( '.portfolio' );
-      
-      $( '.portfolio-wrapper' ).html( portfolio );
-      window.history.pushState({href: href}, '', link);
-    });
-
+    let link = $( this ).attr( 'href' );
+    openUrl(link)
   });
 
+  //for back and front browser arrow event
+  window.addEventListener('popstate', function(e){
+    if( e.state !== null){
+      let link = e.state.href;
+      openUrl(link, false)
+    }
+  })
+
+  //for default
+  history.replaceState({href: "./"}, 'default')
+
 });
+
